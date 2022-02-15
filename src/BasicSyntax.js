1b12925209685de4392f1f8955a9e2098d2243e2
ms-putilova <mail.putilova@gmail.com>
2022-02-15 21:04:26 +0300

    задание по Basic Syntax

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,25 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+
+    let romanArray = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+    let intArray = [1, 5, 10, 50, 100, 500, 1000];
+
+    let i = 0,
+        j = 0;
+    let n = str.length;
+
+    for (i = n - 1; i >= 0; i--) {
+        for (j = 0; j < 7; j++) {
+            if (romanArray[j] == str[i]) {
+                if (intArray[j] > result || str[i] == str[i + 1]) {
+                    result += intArray[j];
+                } else {
+                    result -= intArray[j];
+                }
+            }
+        }
+    }
+
     return result;
 }
